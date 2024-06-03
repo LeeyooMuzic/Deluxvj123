4c99c77ff7e924dc929dfa0b5a117ae68fced2c1
LoveHeart <113758022+LeeyooMuzic@users.noreply.github.com>
2024-06-03 12:41:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ URL = environ.get("URL", "https://mytestvj-998c9929dc7a.herokuapp.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
-RENAME_MODE = bool(environ.get('RENAME_MODE', False)) # Set True or False
+RENAME_MODE = bool(environ.get('RENAME_MODE', True)) # Set True or False
 
 # Auto Approve Info : If True Then Bot Approve New Upcoming Join Request Else Not
 AUTO_APPROVE_MODE = bool(environ.get('AUTO_APPROVE_MODE', False)) # Set True or False
